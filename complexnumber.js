907b0c5bb71618217ab71a78e59371ff010d2a01
Daniel Breno <danielbrenom@gmail.com>
2015-06-17 01:09:08 -0300

    Adicionado reconhecimento de numeros complexos e pagina para utilizar via console.

--- complexnumber.js
+++ complexnumber.js
@@ -12,20 +12,44 @@
    See the License for the specific language governing permissions and
    limitations under the License.
    */
+   function numComplex(x,y)   {
+      this.real = x;
+      this.imag = y;
+      this.signal = (y>=0 ? "+" : "");
+      this.eq = String(x) + this.signal + String(y) + "i";
+   }
+   function searchComplex(input)
+   {
+   	var fullComplex = /([+-]?\d+)\s*([+-])?\s*([+-]?\d+)i/.exec(input);
+   	var imaginary = /(^s*[+-]?\d+i)/.exec(input);
+      var real = /([+-]?\d+)/.exec(input);
+      var complexI = /([-+]?\d*)\s*([-+])?\s*i/.exec(input);
+
+      if(typeof input == "number")
+      {
+         var temp = new numComplex(input,0);      
+         return temp.eq;
+      }
+      else if(typeof input == "string" && fullComplex)
+      {
+         var temp = new numComplex(parseFloat(fullComplex[1]),parseFloat(fullComplex[3]));        
+         return temp.eq;            
+      }
+      else if(typeof input == "string" && imaginary)
+      {
+         var temp = new numComplex(0,parseFloat(imaginary[1]));
+         return temp.eq;
+      }
+      else if(typeof input == "string" && real && !imaginary && !complexI && !fullComplex)
+      {
+         var temp = new numComplex(parseFloat(real[1]),0);
+         return temp.eq;
+      }
+      else if(typeof input == "string" && complexI &&  !imaginary && !fullComplex)
+      {
+         var temp = new numComplex(complexI[1],"");
+         return temp.eq;
+      }
+   }
+
    
-   function searchComplex(number)
-   {
-   	var fullcomplex = /([+-]?\d+)\s*([+-])?\s*([+-]?\d+)i/.exec(number);
-   	var imaginary = /(^s*[+-]?\d+i)/.exec(number);
-   }
-
-
-
-
-   function numComplex(x,y)
-   {
-   	var signal = (y>=0 ? "+" : "");
-   	return String(x)+signal+String(y)+"i";
-   }
-
-   function 
