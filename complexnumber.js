bc3ceebc683f5e76d1d3f6666e0cfc471af153ad
Daniel Breno <danielbrenom@gmail.com>
2015-06-17 23:44:02 -0300

    Corrigido reconhecimento de complexos, adicionadas operaçoes com números: - Adição, subtração, multiplicação, divisão, módulo.

--- complexnumber.js
+++ complexnumber.js
@@ -12,44 +12,123 @@
    See the License for the specific language governing permissions and
    limitations under the License.
    */
-   function numComplex(x,y)   {
+   
+function numComplex(x,y)
+{
       this.real = x;
       this.imag = y;
       this.signal = (y>=0 ? "+" : "");
-      this.eq = String(x) + this.signal + String(y) + "i";
-   }
-   function searchComplex(input)
-   {
+      this.eq = String(x) + this.signal + String(y) + "i";   
+      return this.eq;   
+}
+function newComplex(input)
+{
    	var fullComplex = /([+-]?\d+)\s*([+-])?\s*([+-]?\d+)i/.exec(input);
-   	var imaginary = /(^s*[+-]?\d+i)/.exec(input);
+   	var imaginary = /(^[+-]?\d+i)/.exec(input);
       var real = /([+-]?\d+)/.exec(input);
-      var complexI = /([-+]?\d*)\s*([-+])?\s*i/.exec(input);
+      var complexI = /([+-]?\d+)\s*([-+])?\s*i/.exec(input);
 
       if(typeof input == "number")
       {
-         var temp = new numComplex(input,0);      
-         return temp.eq;
+         var temp = new numComplex(input,0);
+         return temp;
       }
       else if(typeof input == "string" && fullComplex)
       {
-         var temp = new numComplex(parseFloat(fullComplex[1]),parseFloat(fullComplex[3]));        
-         return temp.eq;            
+         if(fullComplex[2]=="-") fullComplex[3]= -parseFloat(fullComplex[3]);
+         var temp = new numComplex(parseFloat(fullComplex[1]),parseFloat(fullComplex[3])); 
+         return temp;         
       }
       else if(typeof input == "string" && imaginary)
       {
+         if(imaginary[0]=="-") imaginary[1]=-parseFloat(imaginary[1]);
          var temp = new numComplex(0,parseFloat(imaginary[1]));
-         return temp.eq;
+         return temp;
       }
       else if(typeof input == "string" && real && !imaginary && !complexI && !fullComplex)
       {
          var temp = new numComplex(parseFloat(real[1]),0);
-         return temp.eq;
+         return temp;
       }
-      else if(typeof input == "string" && complexI &&  !imaginary && !fullComplex)
+      else if(typeof input == "string" && complexI &&  !imaginary && !fullComplex && !real)
       {
-         var temp = new numComplex(complexI[1],"");
-         return temp.eq;
+         var i = (complexI[1]=="-"? -1: 1)
+         var temp = new numComplex(complexI[1],parseFloat(i));
+         return temp;
       }
-   }
-
-   
+}
+function complexSum(first,second)
+{      
+      var a = newComplex(first),
+          b = newComplex(second),
+          r = a.real + b.real,
+          i = a.imag + b.imag;
+          return numComplex(r,i);
+}
+numComplex.prototype.complexSum = function(second)
+{
+   var a = this,
+       b = new newComplex(second),
+       r = a.real + b.real,
+       i = a.imag + b.imag;
+       return numComplex(r,i);
+}
+function complexSub(first,second)
+{
+   var a = newComplex(first),
+       b = newComplex(second),
+       r = a.real - b.real,
+       i = a.imag - b.imag;
+       return numComplex(r,i);
+}
+numComplex.prototype.complexSub = function(second)
+{
+   var a = this,
+       b = new newComplex(second),
+       r = a.real - b.real,
+       i = a.imag - b.imag;
+       return numComplex(r,i);
+}
+function complexMult(first,second)
+{
+   var a = newComplex(first),
+       b = newComplex(second),
+       r = a.real*b.real - (a.imag*b.imag),
+       i = a.real*b.imag + a.imag*b.real;
+       return numComplex(r,i);
+}
+numComplex.prototype.complexMult = function(second)
+{
+   var a = this,
+       b = new newComplex(second),
+       r = a.real*b.real - (a.imag*b.imag),
+       i = a.real*b.imag + a.imag*b.real;
+       return numComplex(r,i);
+}
+function complexDiv(first,second)
+{
+   var a = newComplex(first),
+       b = newComplex(second),
+       r = (a.real*b.real + a.imag*b.imag)/(b.real*b.real+b.imag*b.imag),
+       i = (a.imag*b.real + a.real*b.imag)/(b.real*b.real+b.imag*b.imag);
+       return numComplex(r,i);
+}
+numComplex.prototype.complexDiv = function(second)
+{
+   var a = this,
+       b = new newComplex(second),
+       r = (a.real*b.real + a.imag*b.imag)/(b.real*b.real+b.imag*b.imag),
+       i = (a.imag*b.real + a.real*b.imag)/(b.real*b.real+b.imag*b.imag);
+       return numComplex(r,i);
+}
+function complexMod(input)
+{
+   var a = newComplex(input),
+       Mod = Math.hypot(a.real,a.imag);
+       return Mod;    
+}
+numComplex.prototype.complexMod = function()
+{
+   var Mod = Math.hypot(this.real,this.imag);
+       return Mod;
+}
